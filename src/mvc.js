7f6d4c0bf79a5125bcf00284d49ef3a1c6f15f20
rafa1510 <rcestti78@gmail.com>
2024-01-20 00:52:58 -0500

    Create dialog/form for new tasks

--- src/mvc.js
+++ src/mvc.js
@@ -42,15 +42,17 @@ class task {
 */
 
 // View
-class todoView {
+export default class todoView {
     // Creates our basic HTML elements, both dialogs for creating projects/tasks, the nav, and the main section
     constructor() {
         this.body = document.querySelector("body");
         this.createProjectDialog();
+        this.createTaskDialog();
     }
 
     createProjectDialog() {
         const newDialog = document.createElement("dialog");
+        newDialog.setAttribute("class", "projectDialog");
         const newForm = document.createElement("form");
 
         // Create title label & input
@@ -66,9 +68,98 @@ class todoView {
         const submitButton = document.createElement("button");
         submitButton.textContent = "Submit";
         submitButton.setAttribute("type", "button");
+        submitButton.setAttribute("class", "projectSubmitButton");
 
         // Append title label & submit button to newForm
         newForm.appendChild(titleLabel);
+        newForm.appendChild(submitButton);
+
+        // Append our form to our dialog
+        newDialog.appendChild(newForm);
+
+        // Append our dialog to the body
+        this.body.appendChild(newDialog);
+    }
+
+    createTaskDialog() {
+        const newDialog = document.createElement("dialog");
+        newDialog.setAttribute("class", "taskDialog");
+        const newForm = document.createElement("form");
+
+        // Create title label & input
+        const titleLabel = document.createElement("label");
+        titleLabel.textContent = "Title";
+
+        const titleInput = document.createElement("input");
+        titleInput.setAttribute("type", "text");
+
+        titleLabel.appendChild(titleInput);
+
+        // Create description label & input
+        const descriptionLabel = document.createElement("label");
+        descriptionLabel.textContent = "Description";
+
+        const descriptionInput = document.createElement("input");
+        descriptionInput.setAttribute("type", "text");
+
+        descriptionLabel.appendChild(descriptionInput);
+
+        // Create due date label & input
+        const dateLabel = document.createElement("label");
+        dateLabel.textContent = "Due Date";
+
+        const dateInput = document.createElement("input");
+        dateInput.setAttribute("type", "date");
+
+        dateLabel.appendChild(dateInput);
+
+        // Create priority label, select, & options
+        const priorityLabel = document.createElement("label");
+        priorityLabel.textContent = "Priority";
+
+        const prioritySelect = document.createElement("select");
+        prioritySelect.setAttribute("class", "prioritySelect");
+
+        const priorityOptionOne = document.createElement("option");
+        priorityOptionOne.textContent = "!";
+        priorityOptionOne.setAttribute("value", "!");
+
+        const priorityOptionTwo = document.createElement("option");
+        priorityOptionTwo.textContent = "!!";
+        priorityOptionTwo.setAttribute("value", "!!");
+
+        const priorityOptionThree = document.createElement("option");
+        priorityOptionThree.textContent = "!!!";
+        priorityOptionThree.setAttribute("value", "!!!");
+
+        // Append our three options to our select element
+        prioritySelect.appendChild(priorityOptionOne);
+        prioritySelect.appendChild(priorityOptionTwo);
+        prioritySelect.appendChild(priorityOptionThree);
+
+        priorityLabel.appendChild(prioritySelect);
+
+        // Create completed label & input
+        const completedLabel = document.createElement("label");
+        completedLabel.textContent = "Completed?";
+
+        const completedInput = document.createElement("input");
+        completedInput.setAttribute("type", "checkbox");
+
+        completedLabel.appendChild(completedInput);
+
+        // Create submit button
+        const submitButton = document.createElement("button");
+        submitButton.textContent = "Submit";
+        submitButton.setAttribute("type", "button");
+        submitButton.setAttribute("class", "projectSubmitButton");
+
+        // Append title label, description label, due date label, priority label, completed label & submit button to newForm
+        newForm.appendChild(titleLabel);
+        newForm.appendChild(descriptionLabel);
+        newForm.appendChild(dateLabel);
+        newForm.appendChild(priorityLabel);
+        newForm.appendChild(completedLabel);
         newForm.appendChild(submitButton);
 
         // Append our form to our dialog
